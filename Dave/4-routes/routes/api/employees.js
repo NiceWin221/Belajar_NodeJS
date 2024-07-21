328b0178443ea4b80c0fae1b37fc3eff778a10e6
NiceWin <saputrawindybintan@gmail.com>
2024-07-21 15:56:26 +0700

    routes

---
+++ Dave/4-routes/routes/api/employees.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { route } = require("../subdir");
+const router = express.Router();
+const data = {};
+data.employees = require("../../model/employees.json");
+
+router
+  .route("/")
+  .get((req, res) => {
+    res.json(data.employees);
+  })
+  .post((req, res) => {
+    console.log(req.body);
+    res.json({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+    });
+  })
+  .put((req, res) => {
+    res.json({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+    });
+  })
+  .delete((req, res) => {
+    res.json({ id: req.body.id });
+  });
+
+router.route("/:id").get((req, res) => {
+  res.json({ id: req.params.id });
+});
+
+module.exports = router;
---
+++ Dave/4-routes/server.js
@@ -0,0 +1,61 @@
+const exp = require("constants");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const path = require("path");
+const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
+
+// built-in for json
+app.use(express.json());
+
+// custom middleware logger
+app.use(logger);
+
+// Cross Origin Resource Sharing
+const whitelist = ["https://www.yoursite.com", "https://127.0.0.1:5500", "https://localhost:3000"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+// middleware
+// built-in middleware to handle urlencoded data
+// in other world, form data:
+// `content-type: application/x-www-form-urlencoded`
+app.use(express.urlencoded({ extended: false }));
+
+// serve static file
+app.use("/", express.static(path.join(__dirname, "/public")));
+app.use("/subdir", express.static(path.join(__dirname, "/public")));
+
+// get router file
+app.use("/", require("./routes/root"));
+app.use("/subdir", require("./routes/subdir"));
+app.use("/employees", require("./routes/api/employees"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 not found" });
+  } else {
+    res.type("txt").send("404 not found");
+  }
+});
+
+// handle CORS error
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
